8e0a7b383da48964ad608a854e907128680b2cf8
Guilherme Kwaczynski <139267201+GuilhermeKwT@users.noreply.github.com>
2025-11-12 09:29:46 -0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:geo_loc/view/parque_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: ParquePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
